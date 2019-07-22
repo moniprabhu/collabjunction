6df3015072b381e32ceca0212c6a36f79c9d7d28
Monishaa <monishaa2701@gmail.com>
2019-07-22 14:27:54 +0530

    frontend modified

--- angular-frontend/src/app/service-provider-registration/service-provider-registration.component.ts
+++ angular-frontend/src/app/service-provider-registration/service-provider-registration.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import subdomain from 'src/assets/jsonfiles/data2.json';
-import skills from 'src/assets/jsonfiles/data4.json';
 import roles from 'src/assets/jsonfiles/data3.json';
 import { RegistrationServiceService } from '../registration-service.service';
 import { Router } from '@angular/router';
@@ -91,7 +89,7 @@ export class ServiceProviderRegistrationComponent implements OnInit {
     this.http.get('./assets/jsonfiles/data2.json').subscribe(
       (data: any) => {
         // this.allDomains = data ;	 // FILL THE ARRAY WITH DATA.
-        // console.log(data)
+        console.log(data)
         data.forEach(e => {
           this.allSubDomains.push(e)
 
@@ -255,9 +253,9 @@ export class ServiceProviderRegistrationComponent implements OnInit {
       domainCtrl: ['', Validators.required]
     });
     this.thirdFormGroup = this._formBuilder.group({
-      subdomain: [''],
-      skill: [''],
-      RoleCtrl: ['', Validators.required],
+      subDomainCtrl: [''],
+      skillCtrl: [''],
+      roleCtrl: ['', Validators.required],
       chargePerHour: ['', Validators.required]
     });
   }
@@ -267,17 +265,20 @@ export class ServiceProviderRegistrationComponent implements OnInit {
   }
 
   register(): void {
-    console.log(this.form);
+   
     let providerData = {
-      name: this.firstFormGroup.controls.FirstName.value,
       emailId: this.secondFormGroup.controls.email.value,
       password: this.secondFormGroup.controls.password.value,
+      name: this.firstFormGroup.controls.FirstName.value,
       domain: this.secondFormGroup.controls.domainCtrl.value,
-      subDomain: this.thirdFormGroup.controls.subdomain.value,
-      // skills: this.thirdFormGroup.controls.skill.value,
-      role: this.thirdFormGroup.controls.RoleCtrl.value,
+      subDomain:this.subDomains,
+      role: this.roles[0],
+      skills: this.skills,
+      about: "this is hard coded data",
       chargePerHour: this.thirdFormGroup.controls.chargePerHour.value
     }
+
+    console.log(providerData, "Required DATA ????");
     this.registrationService.addServiceProvider(providerData);
     this.dialog.open(ModalSuccessComponent);
     this.router.navigateByUrl('/home');
